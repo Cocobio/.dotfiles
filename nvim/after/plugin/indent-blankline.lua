53649afc5306f99ecd8e090df1d560c698c0d53a
cocobio <iosorio@udec.cl>
2023-10-21 16:21:08 -0300

    Update on indent blank line setup (default)

--- nvim/after/plugin/indent-blankline.lua
+++ nvim/after/plugin/indent-blankline.lua
@@ -1,11 +1,1 @@
-vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "eol:↴"
--- vim.opt.listchars:append "blankline:"
-
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    indent_blankline_char = '',
-    show_current_context = true,
-    show_current_context_start = true,
-}
+require("ibl").setup()

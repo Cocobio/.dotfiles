a6ed012f7a9063394dcc7ac015eb64b8e34c75cc
cocobio <iosorio@udec.cl>
2023-10-29 17:26:05 -0300

    Adding motd.

---
+++ Install/motd.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+
+motd="
+  \e[;97m\e[40m \e[2;97;40m\e[0m
+  \e[;43m  \e[;40m \e[0m
+ \e[;40m \e[;107m \e[;40m   \e[0m
+\e[;43m \e[;40m    \e[;43m \e[0m
+
+     \e[;33m󰮯\e[0m \e[1;32m󰊠\e[0m \e[1;34m󰊠\e[0m \e[1;31m󰊠\e[0m \e[1;35m󰊠\e[0m 
+"
+
+echo -e "$motd"

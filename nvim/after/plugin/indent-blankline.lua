70a62ebd5de64e99b192c9561991754368eea0de
cocobio <iosorio@udec.cl>
2023-10-31 01:19:38 -0300

    Fix on nvim dashboard. Indentation was showing.

--- nvim/after/plugin/indent-blankline.lua
+++ nvim/after/plugin/indent-blankline.lua
@@ -1,1 +1,17 @@
-require("ibl").setup()
+require("ibl").setup{
+    -- indent = { char = '' },
+    exclude = { filetypes = {'dashboard'} },
+}
+
+
+--[[ vim.opt.list = true
+-- vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "eol:↴"
+-- vim.opt.listchars:append "blankline:"
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    indent_blankline_char = '',
+    show_current_context = true,
+    show_current_context_start = true,
+} ]]
